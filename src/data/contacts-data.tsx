00514ce0eca7868e233fa78cd3f88c628d73c80c
eugen-2018 <eitsolmd@gmail.com>
2024-12-26 13:25:29 +0200

    Numele brand-ului a fost upgrad-at.

---
+++ src/data/contacts-data.tsx
@@ -0,0 +1,7 @@
+const contacts_data = [
+    {
+        phone_number: "+37379954124",
+        locale_address: "Mitropolit Vaarlam St 63/23, MD-2001, Chisinau",
+        email_address: "info@parkit.md",
+    }
+]
