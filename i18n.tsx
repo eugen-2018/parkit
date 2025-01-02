a043943257b35db036fee9ecbaf68958821cd2fd
eugen-2018 <eitsolmd@gmail.com>
2025-01-02 15:06:52 +0200

    (4) Downgrated to nexjs ^14.2.3. Step 4.

--- i18n.tsx
+++ i18n.tsx
@@ -1,14 +1,14 @@
-import { getRequestConfig } from "next-intl/server";
+// import { getRequestConfig } from "next-intl/server";
 import { notFound } from "next/navigation";
 import {locales} from "./config";
 
-export default getRequestConfig(async ({locale}) => {
+// export default getRequestConfig(async ({locale}) => {
 
 
 
 
 
-    
+
 
 
 
